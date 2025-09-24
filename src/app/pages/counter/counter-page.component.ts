a7f03c4833de90df8a255b93efc5367e18c59699
pavs07 <mwariam.bosuro873@gmail.com>
2025-09-23 21:10:08 -0300

    Se agrega el proyecto

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <h1>Hola mundo</h1>
+    <h2>CounterComponent Page</h2>
+  `,
+})
+export class CounterPageComponent {}
